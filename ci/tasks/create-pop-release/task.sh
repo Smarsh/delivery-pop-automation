bad62d52f720c41b401eca8ef5f1cc5994eae2ca
floriov <vanessaflorio1997@gmail.com>
2022-11-10 16:03:22 +0000

    WIP

--- ci/tasks/create-pop-release/task.sh
+++ ci/tasks/create-pop-release/task.sh
@@ -3,37 +3,64 @@
 set -euo pipefail
 
 REPOS=(
-'concourse-mgmt,pop-release-candidate',
-'ea-uaa-deployment,pop-release-candidate',
-'extraction-microservice,pop-release-candidate',
-'identity-microservice,pop-release-candidate',
-'delivery-ea-tenants-pipelines,pop-release-candidate',
-'ea-key-generation,pop-release-candidate',
-'paas-cf-mgmt-aws,pop-release-candidate',
-'paas-cf-mgmt-aws-nam-mt,pop-release-candidate',
-'elasticsearch-boshrelease-deployments,pop-release-candidate',
-'kafka-boshrelease-2.4.x-deployments,pop-release-candidate',
-'mongodb-boshrelease-3.6.x-deployments,pop-release-candidate',
-'ea-egw-pipelines,pop-release-candidate',
-'ea-e2e-smoke,pop-release-candidate',
-'ea-zookeeper,pop-release-candidate',
-'dataservices-deployment-bootstrap,pop-release-candidate'
+'concourse-mgmt',
+'ea-uaa-deployment',
+'extraction-microservice',
+'identity-microservice',
+'delivery-ea-tenants-pipelines',
+'ea-key-generation',
+'paas-cf-mgmt-aws',
+'paas-cf-mgmt-aws-nam-mt',
+'elasticsearch-boshrelease-deployments',
+'kafka-boshrelease-2.4.x-deployments',
+'mongodb-boshrelease-3.6.x-deployments',
+'ea-egw-pipelines',
+'ea-e2e-smoke',
+'ea-zookeeper',
+'dataservices-deployment-bootstrap'
 )
 
-git --config user.name "CI Bot"
-git --config user.email "ci.bot@smarsh.com"
+source .env
 
-for i in "${REPOS[@]}"
+# Set colours
+GREEN="\e[32m"
+RED="\e[41m\e[37m\e[1m"
+YELLOW="\e[33m"
+WHITE="\e[0m"
+
+function setup_git(){
+	echo -e $GREEN"Setup GH SSH Client"$WHITE
+	mkdir ~/.ssh
+	ssh-keyscan github.com >> ~/.ssh/known_hosts 2>/dev/null;
+
+	cat <<-EOF > ~/.ssh/id_rsa
+	$GIT_PRIVATE_KEY_RSA_BASE64
+	EOF
+
+	chmod 600 ~/.ssh/id_rsa
+
+	git config --global user.name "Concourse CI Bot"
+	git config --global user.email "ci@localhost"
+}
+
+function clone_repo(repo){
+	echo -e $GREEN"Cloning Repo $repo"$WHITE
+	git clone git@github.com:Smarsh/${repo}.git
+}
+
+setup_git
+mkdir build
+cd build
+
+for repo in "${REPOS[@]}"
 do
-  delimiter=","
-  declare -a parts=($(echo -e $i | tr "$delimiter" " "))
-  repo=${parts[0]}
-  branch=${parts[1]}
-  if [repo !== delivery-ea-versions-api] ; then
-    git checkout "${branch}" && git push --set-upstream origin "${branch}" -f
-  else
-    #There is an exception for the ea-versions repo where the force push will be aws-us-west-2-poplite-production-> pop-stable
-  fi
-  git push origin --delete "${branch}"
+  clone_repo(${repo})
+  cd $repo
+  git checkout pop-release-candidate && git pull origin pop-release-candidate && git checkout -b pop-stable
+  git push --set-upstream origin pop-stable -f
+  git branch -d pop-release-candidate
+  git push origin --delete pop-release-candidate
+  cd ..
 done
+
 
