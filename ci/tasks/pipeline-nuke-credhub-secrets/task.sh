25ce385cced7b617de3de2849626b0fdf478a5cb
SachinKainthSmarsh <91120510+SachinKainthSmarsh@users.noreply.github.com>
2022-12-07 11:55:24 +0000

    Update task.sh

--- ci/tasks/pipeline-nuke-credhub-secrets/task.sh
+++ ci/tasks/pipeline-nuke-credhub-secrets/task.sh
@@ -3,8 +3,7 @@
 set -euo pipefail
 : "${CUSTOMER:?CUSTOMER env var must be provided}"
 : "${TIER:?TIER env var must be provided}"
-: "${REGION:?REGION env var must be provided}"
-
+: "${SERVICE_REGION:?SERVICE_REGION env var must be provided}"
 
 # Set colours
 GREEN="\e[32m"
@@ -14,8 +13,8 @@ WHITE="\e[0m"
 
 credhub login --skip-tls-validation
 # This line makes sure the delete always works.
-credhub set -n /concourse/platform-delivery-services/${CUSTOMER}-${REGION}-archive-${TIER}-dummy -t 'value' --value 'dummy'
-credhub find -n ${CUSTOMER}-${REGION}-archive-${TIER} | grep name: | cut -c9- | xargs -I {} credhub delete -n {}
+credhub set -n /concourse/platform-delivery-services/${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}-dummy -t 'value' --value 'dummy'
+credhub find -n ${CUSTOMER}-${SERVICE_REGION}-archive-${TIER} | grep name: | cut -c9- | xargs -I {} credhub delete -n {}
 
 credhub set -n /concourse/platform-delivery-services/${CUSTOMER}-${TIER}-dummy -t 'value' --value 'dummy'
 credhub find -n ${CUSTOMER}-${TIER} | grep name: | cut -c9- | xargs -I {} credhub delete -n {}
