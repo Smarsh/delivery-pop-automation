af7d759e2c14586bf4c3ddc9954f0affc42e5612
SachinKainthSmarsh <91120510+SachinKainthSmarsh@users.noreply.github.com>
2022-12-07 11:58:48 +0000

    Update task.sh

--- ci/tasks/pipeline-nuke-bosh-deployments/task.sh
+++ ci/tasks/pipeline-nuke-bosh-deployments/task.sh
@@ -4,24 +4,23 @@ set -euo pipefail
 : "${CUSTOMER:?CUSTOMER env var must be provided}"
 : "${TIER:?TIER env var must be provided}"
 
-
 # Set colours
 GREEN="\e[32m"
 RED="\e[41m\e[37m\e[1m"
 YELLOW="\e[33m"
 WHITE="\e[0m"
 
-bosh_customer_exists=$(bosh deps --column name | grep ${CUSTOMER} >>/dev/null 2>&1;echo $?)
+bosh_customer_exists=$(bosh deps --column name | grep ${CUSTOMER}-${TIER} >>/dev/null 2>&1;echo $?)
 if [ "$bosh_customer_exists" -eq 0 ];
 then
   echo -e ${GREEN}"Deleting Bosh deployments"${WHITE}
-  bosh deps --column name | grep "${CUSTOMER}-*" | awk '{print $1}' | xargs -I {} bosh -n -d {} delete-deployment
+  bosh deps --column name | grep "${CUSTOMER}-${TIER}-*" | awk '{print $1}' | xargs -I {} bosh -n -d {} delete-deployment
 
-  bosh_deps_deleted=$(bosh deps --column name | grep ${CUSTOMER} >>/dev/null 2>&1;echo $?)
+  bosh_deps_deleted=$(bosh deps --column name | grep ${CUSTOMER}-${TIER} >>/dev/null 2>&1;echo $?)
   if [ "$bosh_deps_deleted" -eq 0 ];
   then
     echo -e ${GREEN}"List of deployments after delete"${WHITE}
-    bosh deps --column name | grep ${CUSTOMER}
+    bosh deps --column name | grep ${CUSTOMER}-${TIER}
     exit 1
   else
     echo -e ${GREEN}"All bosh deployments deleted"${WHITE}
