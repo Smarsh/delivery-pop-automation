e5921f5e0315e90780b8bb6014d705e2db58df2c
Joao Moura <joaopaulo.serrademoura@smarsh.com>
2022-12-14 10:07:35 +0000

    data clusters bosh deployments

--- ci/tasks/pipeline-nuke-bosh-deployments/task.sh
+++ ci/tasks/pipeline-nuke-bosh-deployments/task.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 : "${CUSTOMER:?CUSTOMER env var must be provided}"
 : "${TIER:?TIER env var must be provided}"
+: "${SERVICE_REGION:?TIER env var must be provided}"
 
 # Set colours
 GREEN="\e[32m"
@@ -10,28 +11,24 @@ RED="\e[41m\e[37m\e[1m"
 YELLOW="\e[33m"
 WHITE="\e[0m"
 
-bosh_customer_exists=$(bosh deps --column name | grep ${CUSTOMER}-${TIER} >>/dev/null 2>&1;echo $?)
+echo -e ${GREEN}"Customer: ${CUSTOMER}"${WHITE}
+echo -e ${GREEN}"Tier: ${TIER}"${WHITE}
+echo -e ${GREEN}"SERVICE_REGION: ${SERVICE_REGION}"${WHITE}
+
+
+bosh_customer_exists=$(bosh deps --column name | grep -E  "${CUSTOMER}-${TIER}|${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}" >>/dev/null 2>&1;echo $?)
 if [ "$bosh_customer_exists" -eq 0 ];
 then
   echo -e ${GREEN}"Deleting Bosh deployments"${WHITE}
   #DRY RUN
-  bosh deps --column name | grep "${CUSTOMER}-${TIER}-*" | awk '{print $1}' 
+  bosh deps --column name | grep -E  "${CUSTOMER}-${TIER}|${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}" | awk '{print $1}' 
   # bosh deps --column name | grep "${CUSTOMER}-${TIER}-*" | awk '{print $1}' | xargs -I {} bosh -n -d {} delete-deployment
 
-  bosh_deps_deleted=$(bosh deps --column name | grep ${CUSTOMER}-${TIER} >>/dev/null 2>&1;echo $?)
+  bosh_deps_deleted=$(bosh deps --column name | grep -E  "${CUSTOMER}-${TIER}|${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}" >>/dev/null 2>&1;echo $?)
   if [ "$bosh_deps_deleted" -eq 0 ];
   then
     echo -e ${GREEN}"List of deployments after delete"${WHITE}
-    bosh deps --column name | grep ${CUSTOMER}-${TIER}
-    exit 0 #DRY RUN 1->0
-  else
-    echo -e ${GREEN}"All bosh deployments deleted"${WHITE}
-  fi
-  bosh_deps_deleted=$(bosh deps --column name | grep ${CUSTOMER}-${SERVICE_REGION}-archive-${TIER} >>/dev/null 2>&1;echo $?)
-  if [ "$bosh_deps_deleted" -eq 0 ];
-  then
-    echo -e ${GREEN}"List of deployments after delete"${WHITE}
-    bosh deps --column name |  grep ${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}
+    bosh deps --column name | grep -E  "${CUSTOMER}-${TIER}|${CUSTOMER}-${SERVICE_REGION}-archive-${TIER}"
     exit 0 #DRY RUN 1->0
   else
     echo -e ${GREEN}"All bosh deployments deleted"${WHITE}
