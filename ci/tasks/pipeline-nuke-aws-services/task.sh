4118c9c57d7001100a5ec3f5952b899d4a6a83a1
Niharbabu D <nihar.devathi@smarsh.com>
2022-12-23 11:10:34 +0530

    correcting template path for aws rds services

--- ci/tasks/pipeline-nuke-aws-services/task.sh
+++ ci/tasks/pipeline-nuke-aws-services/task.sh
@@ -19,7 +19,7 @@ mkdir terraform_workspace
 ##UAA Deletion
 cd delivery-pop-automation
 KEY_PATH=`aws s3 ls s3://smarsh-terraform-state-management-${REGION_BUCKET_SUFFIX} --recursive --no-paginate | grep -w ${CUSTOMER} | grep ${TIER} | grep uaa | cut -d " " -f 9`
-envsubst < vars/main_template.json > temp.tf && mv temp.tf terraform_workspace/main.tf
+envsubst < ci/vars/main_template.json > temp.tf && mv temp.tf terraform_workspace/main.tf
 cd ../
 
 cd terraform_workspace
@@ -32,7 +32,7 @@ cd ../
 ##TPS Deletion
 cd delivery-pop-automation
 KEY_PATH=`aws s3 ls s3://smarsh-terraform-state-management-${REGION_BUCKET_SUFFIX} --recursive --no-paginate | grep -w ${CUSTOMER} | grep ${TIER} | grep tps | cut -d " " -f 9`
-envsubst < vars/main_template.json > temp.tf && mv temp.tf terraform_workspace/main.tf
+envsubst < ci/vars/main_template.json > temp.tf && mv temp.tf terraform_workspace/main.tf
 cd ../
 
 cd terraform_workspace
